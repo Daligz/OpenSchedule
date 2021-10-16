cf350beb6c444f6672352552a1fe5521297c881f
Dali <oscardali370@gmail.com>
2021-10-16 14:22:02 -0500

    Test and new data objects added

---
+++ src/main/java/me/upp/dali/openschedule/model/database/recods/ScheduleDataObject.java
@@ -0,0 +1,3 @@
+package me.upp.dali.openschedule.model.database.recods;
+
+public record ScheduleDataObject(int id, String code) { }
---
+++ src/main/java/me/upp/dali/openschedule/model/database/recods/UserDataObject.java
@@ -0,0 +1,3 @@
+package me.upp.dali.openschedule.model.database.recods;
+
+public record UserDataObject(int id, String name, String phone) { }
