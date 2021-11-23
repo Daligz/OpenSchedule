fdd4d1d88d688d5135aef8ee06709e8573170317
Daligz <oscardali370@gmail.com>
2021-11-22 21:24:01 -0600

    Fix account disconnected

--- src/main/java/me/upp/dali/openschedule/controller/MessagesAPI.java
+++ src/main/java/me/upp/dali/openschedule/controller/MessagesAPI.java
@@ -1,6 +1,7 @@
 package me.upp.dali.openschedule.controller;
 
 import it.auties.whatsapp4j.whatsapp.WhatsappAPI;
+import it.auties.whatsapp4j.whatsapp.WhatsappConfiguration;
 import lombok.Getter;
 
 public class MessagesAPI {
@@ -9,6 +10,14 @@ public class MessagesAPI {
     private final WhatsappAPI whatsappAPI;
 
     public MessagesAPI() {
-        this.whatsappAPI = new WhatsappAPI();
+        var configuration = WhatsappConfiguration.builder()
+                .whatsappUrl("wss://web.whatsapp.com/ws") // WhatsappWeb's WebSocket URL
+                .requestTag("OpenSchedule") // The tag used for requests made to WhatsappWeb's WebSocket
+                .description("OpenSchedule") // The description provided to Whatsapp during the authentication process
+                .shortDescription("OS") // An acronym for the description
+                .reconnectWhenDisconnected((reason) -> true) // Determines whether the connection should be reclaimed
+                .async(true) // Determines whether requests sent to whatsapp should be asyncronous or not
+                .build(); // Builds an instance of WhatsappConfiguration
+        this.whatsappAPI = new WhatsappAPI(configuration);
     }
 }
