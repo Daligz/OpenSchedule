bb7599658a932ad29a748d50a6c8e76decc7f2ae
Yoana2000 <yobiz2000@gmail.com>
2022-03-23 10:18:32 -0600

    Modificacion de tablas

--- src/main/java/me/upp/dali/openschedule/controller/client/ClientState.java
+++ src/main/java/me/upp/dali/openschedule/controller/client/ClientState.java
@@ -7,6 +7,7 @@ import me.upp.dali.openschedule.controller.others.AbstractDataMap;
 
 import java.sql.Date;
 import java.sql.Timestamp;
+import me.upp.dali.openschedule.model.database.Database;
 
 public class ClientState extends AbstractDataMap<String, ClientState.Client> {
 
@@ -40,5 +41,13 @@ public class ClientState extends AbstractDataMap<String, ClientState.Client> {
 
     public enum Status {
         NONE, REGISTER_CLIENT, REGISTER_KNOWN_CLIENT, REGISTER_CODE_EXPIRE, REGISTER_FINISHED_TIME, REGISTER_NAME_REQUEST, YES_NO_REQUEST
+    }
+    
+     public void createTables(final Database database) {
+        database.createTable("tbl_user", "userId INTEGER NOT NULL", "name TEXT NOT NULL", "phone TEXT NOT NULL", "PRIMARY KEY(\"userId\" AUTOINCREMENT)");
+        database.createTable("tbl_user_time", "userTimeId INTEGER NOT NULL", "phone TEXT NOT NULL", "userCode INTEGER NOT NULL",
+                "tiempoInicio DATE DEFAULT (datetime('now', 'localtime'))","tiempoFin DATE", "PRIMARY KEY(\"userTimeId\" AUTOINCREMENT)");
+        database.createTable("tbl_config", "configId INTEGER NOT NULL", "id TEXT NOT NULL", "value TEXT NOT NULL", "PRIMARY KEY(\"configId\" AUTOINCREMENT)");
+        database.createTable("tbl_inventory", "inventoryId INTEGER NOT NULL", "name TEXT NOT NULL", "admissionDate DATE DEFAULT (datetime('now', 'localtime'))", "state TEXT NOT NULL", "cost REAL NOT NULL", "PRIMARY KEY(\"inventoryId\" AUTOINCREMENT)");
     }
 }
